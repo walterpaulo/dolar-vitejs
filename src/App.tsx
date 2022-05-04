d7a0df4b1a382bdfec41e6dcbc797a741cf2479e
walterpaulo <walter0paulo@hotmail.com>
2022-05-03 22:56:02 -0300

    feat: added bottun

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
 import './App.css'
 
+
+type AcaoProps = {
+  concluida?: boolean
+} & React.ComponentProps<'button'>
+
+function Acao({ concluida, ...props}: AcaoProps) {
+  return <button {...props}>{ concluida ? ' ✅' : ' ❌'}</button>
+}
+
 function App() {
   // const H1 = (args: { children: React.ReactNode}) => {
   //   return <h1>{args.children}</h1>
-const tarefas = [
+
+
+const [tarefas, setTarefas] = useState([
   {
     id: 1,
   nome: 'reatec',
@@ -15,8 +26,19 @@ const tarefas = [
   nome: 'reatec',
   concluida: false
   }
-]
-  
+])
+
+const marcarComoConcluida = (id: number) => {
+  setTarefas(tarefas.map(tarefa => {
+    if (tarefa.id == id) {
+      return {
+        ...tarefa,
+        concluida: !tarefa.concluida
+      }
+    }
+    return tarefa
+  }))
+}
 
   return (
    <div>
@@ -26,7 +48,12 @@ const tarefas = [
        {tarefas.map(tarefa => (
          <li key={tarefa.id}>
            {tarefa.nome}
-           {tarefa.concluida ? ' ✅' : ' ❌'}
+           <Acao 
+            concluida={tarefa.concluida}
+            onClick={() => {
+              marcarComoConcluida(tarefa.id)
+            }}
+           />
          </li>
        ))}
      </ul>
