b2bdb2bde0945e6340e665f94d72fa371f92dc27
walterpaulo <walter0paulo@hotmail.com>
2022-05-03 22:36:25 -0300

    chore: added tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const H1 = (args: { children: React.ReactNode}) => {
+  //   return <h1>{args.children}</h1>
+const tarefas = [
+  {
+    id: 1,
+  nome: 'reatec',
+  concluida: false
+  },
+  {
+    id: 2,
+  nome: 'reatec',
+  concluida: false
+  }
+]
+  
 
   return (
-    <div className="App">
-     ok
-    </div>
+   <div>
+     <h1>Tarefas</h1>
+
+     <ul>
+       {tarefas.map(tarefa => (
+         <li key={tarefa.id}>
+           {tarefa.nome}
+           {tarefa.concluida ? ' ✅' : ' ❌'}
+         </li>
+       ))}
+     </ul>
+   </div>
   )
 }
 
