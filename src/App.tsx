f694551dbcf5b910d516b418e6033fa95dad399a
walterpaulo <walter0paulo@hotmail.com>
2022-06-03 10:14:59 -0300

    refactor: code

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,11 @@
-import React, { useState } from 'react'
-import './App.css'
-
-
-type AcaoProps = {
-  concluida?: boolean
-} & React.ComponentProps<'button'>
-
-function Acao({ concluida, ...props}: AcaoProps) {
-  return <button {...props}>{ concluida ? ' ✅' : ' ❌'}</button>
-}
+import React from 'react';
 
 function App() {
-  // const H1 = (args: { children: React.ReactNode}) => {
-  //   return <h1>{args.children}</h1>
-
-
-const [tarefas, setTarefas] = useState([
-  {
-    id: 1,
-  nome: 'reatec',
-  concluida: false
-  },
-  {
-    id: 2,
-  nome: 'reatec',
-  concluida: false
-  }
-])
-
-const marcarComoConcluida = (id: number) => {
-  setTarefas(tarefas.map(tarefa => {
-    if (tarefa.id == id) {
-      return {
-        ...tarefa,
-        concluida: !tarefa.concluida
-      }
-    }
-    return tarefa
-  }))
-}
-
   return (
-   <div>
-     <h1>Tarefas</h1>
-
-     <ul>
-       {tarefas.map(tarefa => (
-         <li key={tarefa.id}>
-           {tarefa.nome}
-           <Acao 
-            concluida={tarefa.concluida}
-            onClick={() => {
-              marcarComoConcluida(tarefa.id)
-            }}
-           />
-         </li>
-       ))}
-     </ul>
-   </div>
-  )
+    <div>
+      oi
+    </div>
+  );
 }
 
-export default App
+export default App;
