c5b6b9dca234f9bb6ff0ee1f69ed029ad0065cad
Walter Paulo <walter0paulo@hotmail.com>
2022-06-07 01:41:49 -0300

    Update index.tsx
    fix: link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Container, Span } from './style';
 function Footer() {
     return (
         <Container>
-            <Span>&#169; 2022. Website Designed by <a href='https://wwww.walterpaulo.com.br' target="_blank">walter Paulo</a></Span>
+            <Span>&#169; 2022. Website Designed by <a href='https://www.walterpaulo.com.br' target="_blank">walter Paulo</a></Span>
         </Container>        
     );
 }
