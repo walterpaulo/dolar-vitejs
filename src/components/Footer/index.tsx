bc6753485cf8adb98b18dbc5e32601fa039fd573
walterpaulo <walter0paulo@hotmail.com>
2022-06-07 01:39:46 -0300

    fix: link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Container, Span } from './style';
 function Footer() {
     return (
         <Container>
-            <Span>&#169; 2022. Website Designed by <a href='https://wwww.walterpaulo.com.br' target="_blank">Walter Paulo</a></Span>
+            <Span>&#169; 2022. Website Designed by <a href='https://www.walterpaulo.com.br' target="_blank">Walter Paulo</a></Span>
             <Span className='accessCod'>&#10;<a href='https://github.com/walterpaulo/dolar-vitejs' target='_blank'>&amp;&#32;código fonte&#32;</a></Span>
         </Container>        
     );
